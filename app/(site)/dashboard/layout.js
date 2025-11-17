c7a678de3a1b584516dcc350cd814fa220eda77a
AstroJaggaDaku <iamastrojd@gmail.com>
2025-11-17 17:56:07 +0530

    Update layout.js

--- app/(site)/dashboard/layout.js
+++ app/(site)/dashboard/layout.js
@@ -1,8 +1,13 @@
 export default function DashboardLayout({ children }) {
   return (
     <div className="flex min-h-screen">
-      <aside className="w-64 bg-white shadow p-4">Dashboard Menu</aside>
-      <div className="flex-1 p-6">{children}</div>
+      <aside className="w-64 bg-white shadow p-4">
+        Dashboard Menu
+      </aside>
+
+      <main className="flex-1 p-6">
+        {children}
+      </main>
     </div>
   );
 }
