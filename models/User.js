edfd782f7ed0545869e620d6f61ffd01cc394586
AstroJaggaDaku <iamastrojd@gmail.com>
2025-11-17 18:20:47 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-  image: String,
-  role: { type: String, default: "user" },
-  canViewProfiles: { type: Boolean, default: false },
-  premium: { type: Boolean, default: false },
-  premiumExpires: { type: Date, default: null }
-});
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+
+    email: { type: String, required: true, unique: true },
+
+    password: { type: String, required: true },
+
+    role: { type: String, default: "user" },
+
+    premium: { type: Boolean, default: false },
+
+    canViewProfiles: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
